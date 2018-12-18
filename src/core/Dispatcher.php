8189c3f906d1f6cab63dbc161631af4b800d7874
Daniel Werner <vernerd@gmail.com>
2018-12-18 22:17:33 +0100

    route class, router params and refactor

--- src/core/Dispatcher.php
+++ src/core/Dispatcher.php
@@ -2,14 +2,17 @@
 
 namespace Microframe\Core;
 
+use Microframe\Routing\Router;
+
 class Dispatcher
 {
     public static function dispatch(){
-        list($controller, $action) = $params = Router::getParams();
+        list($controller, $action, $routerParams) = $params = Router::getParams();
         $controller = '\Controllers\\' . $controller;
         $action = new \ReflectionMethod($controller, $action);
 
         $params = !empty($_POST) ? $_POST : $_GET;
+        $params = array_merge($params, $routerParams);
 
         return $action->invoke( new $controller, $params );
     }
--- tests/DispatcherTest.php
+++ tests/DispatcherTest.php
@@ -2,12 +2,33 @@
 use PHPUnit\Framework\TestCase;
 use Microframe\Core\Dispatcher;
 use Microframe\Core\View;
+use Microframe\Routing\Routes;
 
 class DispatcherTest extends TestCase
 {
+    public function setUp()
+    {
+        parent::setUp();
+
+        Routes::get('/test/new', 'TestController@add');
+        Routes::post('/test/new', 'TestController@store');
+        Routes::get('/test/param/{id}', 'TestController@add');
+    }
+
     public function testDispatch()
     {
         $_SERVER['REQUEST_URI'] = '/test/new';
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+
+        $view = Dispatcher::dispatch();
+
+        $this->assertInstanceOf( View::class, $view );
+
+    }
+
+    public function testDispatchWithParams()
+    {
+        $_SERVER['REQUEST_URI'] = '/test/param/1';
         $_SERVER['REQUEST_METHOD'] = 'GET';
 
         $view = Dispatcher::dispatch();
